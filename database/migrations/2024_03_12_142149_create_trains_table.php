76166477f8f6f361adc64dfc12a2c1709935530b
FrancescoM95 <mon95fran@gmail.com>
2024-03-12 15:47:01 +0100

    created trains_table

---
+++ database/migrations/2024_03_12_142149_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('cp', 5)->unique();
+            $table->tinyInteger('wagons')->unsigned();
+            $table->boolean('isInTime')->default('1');
+            $table->boolean('isDeleted')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
